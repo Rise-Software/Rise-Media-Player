f001f5c43dd80eb0ce419d3cf4061ea4dc2bec32
Omar Plata Salas <omarps464@gmail.com>
2021-11-28 17:44:59 -0500

    improved video tooltips and at a glance

--- Rise Media Player Dev/Converters/TimeSpanToString.cs
+++ Rise Media Player Dev/Converters/TimeSpanToString.cs
@@ -5,6 +5,71 @@ namespace Rise.App.Converters
 {
     public class TimeSpanToString : IValueConverter
     {
+        public static string Convert(TimeSpan value, string param = null)
+        {
+            if (param == null)
+            {
+                return value.ToString("mm\\:ss");
+            }
+
+            string time = "";
+            switch (param)
+            {
+                case "D-S":
+                    if (value.Days > 0)
+                    {
+                        if (value.Days == 1)
+                        {
+                            time += value.Days + " day, ";
+                        }
+                        else
+                        {
+                            time += value.Days + " days, ";
+                        }
+                    }
+
+                    if (value.Hours > 0)
+                    {
+                        if (value.Hours == 1)
+                        {
+                            time += value.Hours + " hour, ";
+                        }
+                        else
+                        {
+                            time += value.Hours + " hours, ";
+                        }
+                    }
+
+                    if (value.Minutes > 0)
+                    {
+                        if (value.Minutes == 1)
+                        {
+                            time += value.Minutes + " minute, ";
+                        }
+                        else
+                        {
+                            time += value.Minutes + " minutes, ";
+                        }
+                    }
+
+                    if (value.Seconds == 1)
+                    {
+                        time += value.Seconds + " second";
+                    }
+                    else
+                    {
+                        time += value.Seconds + " seconds";
+                    }
+                    break;
+
+                default:
+                    time = value.ToString("mm\\:ss");
+                    break;
+            }
+
+            return time;
+        }
+
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             TimeSpan span = (TimeSpan)value;
@@ -13,62 +78,7 @@ namespace Rise.App.Converters
                 return span.ToString("mm\\:ss");
             }
 
-            string time = "";
-            switch (parameter.ToString())
-            {
-                case "D-S":
-                    if (span.Days > 0)
-                    {
-                        if (span.Days == 1)
-                        {
-                            time += span.Days + " day, ";
-                        }
-                        else
-                        {
-                            time += span.Days + " days, ";
-                        }
-                    }
-
-                    if (span.Hours > 0)
-                    {
-                        if (span.Hours == 1)
-                        {
-                            time += span.Hours + " hour, ";
-                        }
-                        else
-                        {
-                            time += span.Hours + " hours, ";
-                        }
-                    }
-
-                    if (span.Minutes > 0)
-                    {
-                        if (span.Minutes == 1)
-                        {
-                            time += span.Minutes + " minute, ";
-                        }
-                        else
-                        {
-                            time += span.Minutes + " minutes, ";
-                        }
-                    }
-
-                    if (span.Seconds == 1)
-                    {
-                        time += span.Seconds + " second";
-                    }
-                    else
-                    {
-                        time += span.Seconds + " seconds";
-                    }
-                    break;
-
-                default:
-                    time = span.ToString("mm\\:ss");
-                    break;
-            }
-
-            return time;
+            return Convert(span, parameter.ToString());
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
