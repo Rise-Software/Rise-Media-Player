1e2699e6f19faf63bb2564600ece53c4d9028652
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2021-12-09 22:13:17 +0200

    Add crash handler

---
+++ Rise Media Player Dev/Windows/CrashDetailsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Rise.App.Common;
+using Rise.App.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Rise.App.Views
+{
+    public sealed partial class CrashDetailsPage : Page
+    {
+        public static CrashDetailsPage Current;
+        private string Text = "";
+
+        public CrashDetailsPage()
+        {
+            InitializeComponent();
+            Current = this;
+            Loaded += CrashDetailsPage_Loaded;
+        }
+
+        private void CrashDetailsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            _ = new ApplicationTitleBar(AppTitleBar);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Text = e.Parameter as string;
+            base.OnNavigatedTo(e);
+        }
+
+        private void SubmitIssueButton_Click(object sender, RoutedEventArgs e) => _ = URLs.Feedback.LaunchAsync();
+    }
+}
