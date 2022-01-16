0f5c72bcd167b1a5361c12bb049adff210de8a86
Erick Amezcua <erickamezcua12@gmail.com>
2022-01-16 11:21:20 -0700

    Add functionality to Edit Album Art button

--- Rise Media Player Dev/Props/DetailsPage.xaml.cs
+++ Rise Media Player Dev/Props/DetailsPage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Rise.App.ViewModels;
+﻿using Rise.App.Common;
+using Rise.App.ViewModels;
+using System;
+using Windows.Storage.FileProperties;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 namespace Rise.App.Props
@@ -28,6 +33,34 @@ namespace Rise.App.Props
         private void LocalButton_Click(object sender, RoutedEventArgs e)
         {
             LocalTip.IsOpen = true;
+        }
+
+        private async void SplitButton_Click(Microsoft.UI.Xaml.Controls.SplitButton sender, Microsoft.UI.Xaml.Controls.SplitButtonClickEventArgs args)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary
+            };
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+            if(file != null)
+            {
+                // Get file thumbnail and make a PNG out of it.
+                StorageItemThumbnail thumbnail = await file.GetThumbnailAsync(ThumbnailMode.MusicView, 200);
+                await FileHelpers.SaveBitmapFromThumbnailAsync(thumbnail, $@"modified-artist-{file.Name}.png");
+
+                var uri = new Uri($@"ms-appdata:///local/modified-artist-{file.Name}.png");
+
+                thumbnail.Dispose();
+                Props.Thumbnail = uri.ToString();
+                //Update the source in the XAML view
+                imgAlbum.Source = new BitmapImage(uri);
+            }
         }
     }
 }
