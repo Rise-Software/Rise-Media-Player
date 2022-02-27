abf6fc14e9a1fc2802a2e328057e2b245cced555
Omar Plata Salas <omarps464@gmail.com>
2022-02-27 15:59:32 -0500

    created indexing helpers class

---
+++ Rise Media Player Dev/Indexing/IndexingHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rise.App.Indexing
+{
+    public static class IndexingHelpers
+    {
+    }
+}
