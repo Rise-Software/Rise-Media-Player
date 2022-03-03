1180b5207bf20addc4df3143c8f38a5d67a4681d
Omar Plata Salas <omarps464@gmail.com>
2022-03-03 18:21:47 -0500

    added basic change tracking code

---
+++ Rise.Tasks/MusicIndexingTask.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+
+namespace Rise.Tasks
+{
+    public sealed class MusicIndexingTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral deferral;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.deferral = taskInstance.GetDeferral();
+
+            var tracker = await GetChangeTrackerAsync();
+            await ProcessChangesAsync(tracker);
+
+            this.deferral.Complete();
+        }
+
+        private async Task<StorageLibraryChangeTracker> GetChangeTrackerAsync()
+        {
+            StorageLibrary library = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Music);
+            library.ChangeTracker.Enable();
+
+            return library.ChangeTracker;
+        }
+
+        private async Task ProcessChangesAsync(StorageLibraryChangeTracker changeTracker)
+        {
+            var reader = changeTracker.GetChangeReader();
+            var changeSet = await reader.ReadBatchAsync();
+
+            foreach (StorageLibraryChange change in changeSet)
+            {
+                if (change.ChangeType == StorageLibraryChangeType.ChangeTrackingLost)
+                {
+                    changeTracker.Reset();
+                    return;
+                }
+
+                if (change.IsOfType(StorageItemTypes.Folder))
+                {
+
+                }
+                else if (change.IsOfType(StorageItemTypes.File))
+                {
+
+                }
+                else if (change.IsOfType(StorageItemTypes.None))
+                {
+                    if (change.ChangeType == StorageLibraryChangeType.Deleted)
+                    {
+
+                    }
+                }
+            }
+
+            await reader.AcceptChangesAsync();
+        }
+    }
+}
