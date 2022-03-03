777b8340b8efc12c44824106805bd200c20dfb59
Omar Plata Salas <omarps464@gmail.com>
2022-03-03 17:50:05 -0500

    added tasks and data projects

---
+++ Rise.Tasks/IndexingTask.cs
@@ -0,0 +1,16 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace Rise.Tasks
+{
+    public sealed class IndexingTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral deferral;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.deferral = taskInstance.GetDeferral();
+
+            this.deferral.Complete();
+        }
+    }
+}
