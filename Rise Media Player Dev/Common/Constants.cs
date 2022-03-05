13b135d0b92068c4b48e0f7c65dcb8fa7d0b1daa
Omar Plata Salas <omarps464@gmail.com>
2022-03-05 15:26:49 -0500

    added new blank artist thumbnails

--- Rise Media Player Dev/Common/Constants.cs
+++ Rise Media Player Dev/Common/Constants.cs
@@ -193,6 +193,6 @@
         /// contain one already.
         /// </summary>
         public const string ArtistThumb =
-            "ms-appx:///Assets/missingperson.png";
+            "ms-appx:///Assets/BlankArtist.png";
     }
 }
