538bf93156db08c844c200194ea87d962803dd25
Omar Plata Salas <omarps464@gmail.com>
2022-04-07 20:07:17 -0500

    Started work on new media bar and playback data source

---
+++ Rise Media Player Dev/UserControls/RiseMediaBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Rise.App.UserControls
+{
+    public sealed partial class RiseMediaBar : UserControl
+    {
+        public RiseMediaBar()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Rise.Data/Sources/MediaPlaybackDataSource.cs
@@ -0,0 +1,10 @@
+﻿namespace Rise.Data.Sources
+{
+    /// <summary>
+    /// This class contains properties and methods that
+    /// help when it comes to controlling media playback.
+    /// </summary>
+    public class MediaPlaybackDataSource
+    {
+    }
+}
