4a58dbbf3e8d6be0e0f63266cba7cea71d6b388a
Omar Plata Salas <omarps464@gmail.com>
2022-04-07 20:28:44 -0500

    Added IMediaItem interface

---
+++ Rise.Common/Interfaces/IMediaItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media;
+
+namespace Rise.Common.Interfaces
+{
+    /// <summary>
+    /// Defines a set of properties suitable for displaying
+    /// media items.
+    /// </summary>
+    public interface IMediaItem
+    {
+        /// <summary>
+        /// Title of the playback item. Generally a filename, title
+        /// or name.
+        /// </summary>
+        string Title { get; }
+
+        /// <summary>
+        /// Subtitle of the playback item. Generally the author of
+        /// the playback item.
+        /// </summary>
+        string Subtitle { get; }
+
+        /// <summary>
+        /// Thumbnail of the playback item. This is simply an URI
+        /// stored as a string.
+        /// </summary>
+        string Thumbnail { get; }
+
+        /// <summary>
+        /// Type of playback item.
+        /// </summary>
+        MediaPlaybackType ItemType { get; }
+    }
+}
