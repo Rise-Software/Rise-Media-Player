512ea6334d714618f183ae87230ed1f85776251d
Omar Plata Salas <omarps464@gmail.com>
2022-04-11 00:41:49 -0500

    Added derived class from Media Player Element and Transport Controls, with extra templates

---
+++ Rise Media Player Dev/UserControls/RiseMediaPlayerElement.cs
@@ -0,0 +1,37 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Rise.App.UserControls
+{
+    /// <summary>
+    /// Custom media player element implementation for RiseMP.
+    /// </summary>
+    public sealed partial class RiseMediaPlayerElement : MediaPlayerElement
+    {
+        /// <summary>
+        /// Gets or sets the player's visibility.
+        /// </summary>
+        public Visibility MediaPlayerVisibility
+        {
+            get => (Visibility)GetValue(MediaPlayerVisibilityProperty);
+            set => SetValue(MediaPlayerVisibilityProperty, value);
+        }
+    }
+
+    // Dependency Properties
+    public sealed partial class RiseMediaPlayerElement : MediaPlayerElement
+    {
+        public readonly static DependencyProperty MediaPlayerVisibilityProperty =
+            DependencyProperty.Register(nameof(MediaPlayerVisibility), typeof(Visibility),
+                typeof(RiseMediaPlayerElement), new PropertyMetadata(Visibility.Visible));
+    }
+
+    // Constructor
+    public sealed partial class RiseMediaPlayerElement : MediaPlayerElement
+    {
+        public RiseMediaPlayerElement()
+        {
+            this.DefaultStyleKey = typeof(RiseMediaPlayerElement);
+        }
+    }
+}
---
+++ Rise Media Player Dev/UserControls/RiseMediaTransportControls.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Rise.App.UserControls
+{
+    /// <summary>
+    /// Custom media transport controls implementation for RiseMP.
+    /// </summary>
+    public sealed partial class RiseMediaTransportControls : MediaTransportControls
+    {
+        /// <summary>
+        /// Invoked when the shuffle button is clicked. Event arg corresponds
+        /// to the IsChecked value of the ToggleButton.
+        /// </summary>
+        public event EventHandler<bool> ShufflingChanged;
+
+        /// <summary>
+        /// The item to display next to the controls. When using
+        /// compact mode, it gets hidden.
+        /// </summary>
+        public object DisplayItem
+        {
+            get => GetValue(DisplayItemProperty);
+            set => SetValue(DisplayItemProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the <see cref="DisplayItem"/> visibility.
+        /// </summary>
+        public Visibility DisplayItemVisibility
+        {
+            get => (Visibility)GetValue(DisplayItemVisibilityProperty);
+            set => SetValue(DisplayItemVisibilityProperty, value);
+        }
+
+        /// <summary>
+        /// The template for <see cref="DisplayItem"/>.
+        /// </summary>
+        public DataTemplate DisplayItemTemplate
+        {
+            get => (DataTemplate)GetValue(DisplayItemTemplateProperty);
+            set => SetValue(DisplayItemTemplateProperty, value);
+        }
+
+        /// <summary>
+        /// The template selector for <see cref="DisplayItem"/>.
+        /// </summary>
+        public DataTemplateSelector DisplayItemTemplateSelector
+        {
+            get => (DataTemplateSelector)GetValue(DisplayItemTemplateProperty);
+            set => SetValue(DisplayItemTemplateProperty, value);
+        }
+    }
+
+    // Dependency Properties
+    public sealed partial class RiseMediaTransportControls : MediaTransportControls
+    {
+        public readonly static DependencyProperty DisplayItemProperty =
+            DependencyProperty.Register(nameof(DisplayItem), typeof(object),
+                typeof(RiseMediaTransportControls), new PropertyMetadata(null));
+
+        public readonly static DependencyProperty DisplayItemVisibilityProperty =
+            DependencyProperty.Register(nameof(DisplayItemVisibility), typeof(Visibility),
+                typeof(RiseMediaTransportControls), new PropertyMetadata(Visibility.Collapsed));
+
+        public readonly static DependencyProperty DisplayItemTemplateProperty =
+            DependencyProperty.Register(nameof(DisplayItemTemplate), typeof(DataTemplate),
+                typeof(RiseMediaTransportControls), new PropertyMetadata(null));
+
+        public readonly static DependencyProperty DisplayItemTemplateSelectorProperty =
+            DependencyProperty.Register(nameof(DisplayItemTemplateSelector), typeof(DataTemplateSelector),
+                typeof(RiseMediaTransportControls), new PropertyMetadata(null));
+    }
+
+    // Constructor
+    public sealed partial class RiseMediaTransportControls : MediaTransportControls
+    {
+        public RiseMediaTransportControls()
+        {
+            this.DefaultStyleKey = typeof(RiseMediaTransportControls);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            var shuffle = GetTemplateChild("ShuffleButton") as ToggleButton;
+
+            shuffle.Checked += (s, e) => ShufflingChanged?.Invoke(s, true);
+            shuffle.Unchecked += (s, e) => ShufflingChanged?.Invoke(s, false);
+
+            base.OnApplyTemplate();
+        }
+    }
+}
