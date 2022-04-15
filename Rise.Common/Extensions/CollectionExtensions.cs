b393a9efc55f4c1fce09bc92e331b8f810456d39
Omar Plata Salas <omarps464@gmail.com>
2022-04-15 16:10:08 -0500

    Added methods to move items within a list

--- Rise.Common/Extensions/CollectionExtensions.cs
+++ Rise.Common/Extensions/CollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Toolkit.Uwp.UI;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
@@ -6,6 +7,49 @@ namespace Rise.Common.Extensions
 {
     public static class CollectionExtensions
     {
+        /// <summary>
+        /// Move item at oldIndex to newIndex.
+        /// </summary>
+        public static void Move<T>(this IList<T> list, int oldIndex, int newIndex)
+        {
+            T item = list[oldIndex];
+
+            list.RemoveAt(oldIndex);
+            list.Insert(newIndex, item);
+        }
+
+        /// <summary>
+        /// Moves a set of items to the end of the list.
+        /// </summary>
+        /// <param name="items">Items to move.</param>
+        public static void MoveItemsToEnd<T>(this IList<T> list, T[] items)
+        {
+            foreach (var item in items)
+            {
+                int moveFrom = list.IndexOf(item);
+                list.Move(moveFrom, list.Count - 1);
+            }
+        }
+
+        /// <summary>
+        /// Moves the items between <paramref name="startIndex"/> and
+        /// <paramref name="endIndex"/> (inclusive) to the end of the list.
+        /// </summary>
+        public static void MoveRangeToEnd<T>(this IList<T> list,
+            int startIndex, int endIndex)
+        {
+            if (startIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+
+            if (endIndex > list.Count - 1)
+                throw new ArgumentOutOfRangeException(nameof(endIndex));
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                list.Move(startIndex, list.Count - 1);
+            }
+        }
+
         public static bool MoveItemInCollection<T>(this Collection<T> collection, T item, int newIndex)
         {
             if (collection?.Remove(item) ?? false)
