7505b0238c7622ed1fbfba63582d841d620c64a1
Omar Plata Salas <omarps464@gmail.com>
2022-04-24 16:59:08 -0500

    Added new now playing page

---
+++ Rise Media Player Dev/Windows/NowPlayingPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Rise.App.Views
+{
+    public sealed partial class NowPlayingPage : Page
+    {
+        public NowPlayingPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
