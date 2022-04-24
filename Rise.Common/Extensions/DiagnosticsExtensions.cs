06578897d2e0fcd927207d0a38c459047874aee2
Omar Plata Salas <omarps464@gmail.com>
2022-04-24 15:58:03 -0500

    Added DiagnosticsExtensions class

---
+++ Rise.Common/Extensions/DiagnosticsExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Rise.Common.Extensions
+{
+    /// <summary>
+    /// Extensions that help improve the debugging experience.
+    /// </summary>
+    public static class DiagnosticsExtensions
+    {
+        /// <summary>
+        /// Whether the deployed build is a debug one.
+        /// </summary>
+#if DEBUG
+        public static readonly bool IsDebugBuild = true;
+#else
+        public static readonly bool IsDebugBuild = false;
+#endif
+
+        /// <summary>
+        /// Logs an exception's type, HRESULT, source, message, and
+        /// stack trace to output.
+        /// </summary>
+        public static void LogException(this Exception ex)
+        {
+            if (Debugger.IsAttached && IsDebugBuild)
+            {
+                Debug.WriteLine("--- Exception ---");
+                Debug.Write("Exception type: ");
+                Debug.WriteLine(ex.GetType());
+
+                Debug.Write("HRESULT: ");
+                Debug.WriteLine(ex.HResult);
+                Debug.Write("Source: ");
+                Debug.WriteLine(ex.Source);
+
+                Debug.Write("Message: ");
+                Debug.WriteLine(ex.Message);
+                Debug.Write("Stack trace: ");
+                Debug.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
