bf6c37567f614ebf17ac69db35f144375585f612
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-20 21:35:51 -0500

    Added GlazeTypes enum

---
+++ Rise.Common/Enums/GlazeTypes.cs
@@ -0,0 +1,29 @@
+﻿namespace Rise.Common.Enums
+{
+    /// <summary>
+    /// Defines the available window glaze settings.
+    /// </summary>
+    public enum GlazeTypes
+    {
+        /// <summary>
+        /// No glaze is used.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The system accent is used for glaze.
+        /// </summary>
+        AccentColor,
+
+        /// <summary>
+        /// A custom user-defined color is used for glaze.
+        /// </summary>
+        CustomColor,
+
+        /// <summary>
+        /// The current media thumbnail's dominant color is
+        /// used for glaze.
+        /// </summary>
+        MediaTHumbnail
+    }
+}
