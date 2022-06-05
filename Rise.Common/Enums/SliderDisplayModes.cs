41ffa999e0d004797536b31cda8cf908cbbe90cc
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-04 20:25:16 -0500

    Added SliderDisplayModes enum

---
+++ Rise.Common/Enums/SliderDisplayModes.cs
@@ -0,0 +1,33 @@
+﻿namespace Rise.Common.Enums
+{
+    /// <summary>
+    /// Defines the different way transport control sliders
+    /// can be displayed.
+    /// </summary>
+    public enum SliderDisplayModes
+    {
+        /// <summary>
+        /// Neither the slider nor its associated labels
+        /// are displayed.
+        /// </summary>
+        Hidden,
+
+        /// <summary>
+        /// Only the slider bar is shown, no thumb. Associated
+        /// labels are not displayed.
+        /// </summary>
+        Minimal,
+
+        /// <summary>
+        /// Only the slider bar and its thumb is shown.
+        /// Associated labels are not displayed.
+        /// </summary>
+        SliderOnly,
+
+        /// <summary>
+        /// The slider and any associated labels are
+        /// displayed.
+        /// </summary>
+        Full,
+    }
+}
