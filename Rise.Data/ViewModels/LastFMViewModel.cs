be77c507194990863e3a8c07ec4b063df1f5d453
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-17 14:46:26 -0500

    Added session key and username to ViewModel

--- Rise.Data/ViewModels/LastFMViewModel.cs
+++ Rise.Data/ViewModels/LastFMViewModel.cs
@@ -8,5 +8,19 @@ namespace Rise.Data.ViewModels
 {
     public class LastFMViewModel : ViewModel
     {
+        /// <summary>
+        /// Session key used for the LastFM API.
+        /// </summary>
+        private string _sessionKey;
+
+        private string _username;
+        /// <summary>
+        /// Username for the currently logged in user.
+        /// </summary>
+        public string Username
+        {
+            get => _username;
+            private set => Set(ref _username, value);
+        }
     }
 }
