3f178dde1180484ea74d2751a0992602e64a765f
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-07-22 13:04:44 -0500

    Added attached properties to easily add rows and columns to a grid

---
+++ Rise.Common/Attached/GridHelper.cs
@@ -0,0 +1,106 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Rise.Common.Attached
+{
+    /// <summary>
+    /// A set of attached properties that make working with the
+    /// grid system easier.
+    /// </summary>
+    public sealed class GridHelper : DependencyObject
+    {
+        /// <summary>
+        /// A property that contains a string based representation
+        /// of a grid's row heights.
+        /// </summary>
+        public static readonly DependencyProperty RowHeightsProperty =
+            DependencyProperty.RegisterAttached("RowHeights", typeof(string),
+                typeof(GridHelper), new PropertyMetadata(string.Empty, RowHeightsChanged));
+
+        public static string GetRowHeights(Grid obj)
+            => (string)obj.GetValue(RowHeightsProperty);
+        public static void SetRowHeights(Grid obj, string value)
+            => obj.SetValue(RowHeightsProperty, value);
+
+        /// <summary>
+        /// A property that contains a string based representation
+        /// of a grid's column widths.
+        /// </summary>
+        public static readonly DependencyProperty ColumnWidthsProperty =
+            DependencyProperty.RegisterAttached("ColumnWidths", typeof(string),
+                typeof(GridHelper), new PropertyMetadata(string.Empty, ColumnWidthsChanged));
+
+        public static string GetColumnWidths(Grid obj)
+            => (string)obj.GetValue(ColumnWidthsProperty);
+        public static void SetColumnWidths(Grid obj, string value)
+            => obj.SetValue(ColumnWidthsProperty, value);
+
+        private static void RowHeightsChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs args)
+        {
+            if (d is not Grid grid)
+                return;
+
+            grid.RowDefinitions.Clear();
+
+            var definitions = args.NewValue.ToString();
+            if (string.IsNullOrEmpty(definitions))
+                return;
+
+            var heights = definitions.Split(',');
+            foreach (var height in heights)
+                AddDefinition(grid, height, true);
+        }
+
+        private static void ColumnWidthsChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs args)
+        {
+            if (d is not Grid grid)
+                return;
+
+            grid.ColumnDefinitions.Clear();
+
+            var definitions = args.NewValue.ToString();
+            if (string.IsNullOrEmpty(definitions))
+                return;
+
+            var widths = definitions.Split(',');
+            foreach (var width in widths)
+                AddDefinition(grid, width, false);
+        }
+
+        public static void AddDefinition(Grid grid, string definition, bool isRow)
+        {
+            GridLength length;
+            if (definition == "Auto")
+            {
+                length = GridLength.Auto;
+            }
+            else if (definition.EndsWith("*"))
+            {
+                var val = definition.Replace("*", "");
+                if (string.IsNullOrEmpty(val))
+                    val = "1";
+
+                var size = double.Parse(val);
+                length = new GridLength(size, GridUnitType.Star);
+            }
+            else
+            {
+                var size = double.Parse(definition);
+                length = new GridLength(size, GridUnitType.Pixel);
+            }
+
+            if (isRow)
+            {
+                var def = new RowDefinition { Height = length };
+                grid.RowDefinitions.Add(def);
+            }
+            else
+            {
+                var def = new ColumnDefinition { Width = length };
+                grid.ColumnDefinitions.Add(def);
+            }
+        }
+    }
+}
