69548f3257715d383fb2e72603d4d04b6bc54597
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-08-08 21:47:28 +0200

    Edit assembly info of Rise.Effects

--- Rise.Audio/Properties/AssemblyInfo.cs
+++ Rise.Audio/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rise.Audio")]
+[assembly: AssemblyTitle("Rise.Effects")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rise.Audio")]
+[assembly: AssemblyProduct("Rise.Effects")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
