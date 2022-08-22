9dae1b65fd5a856b16eb45778ed60aa6af72aca4
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-08-21 19:48:48 -0500

    Added new queue flyout

---
+++ Rise Media Player Dev/UserControls/DefaultQueueFlyout.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Rise.Common.Interfaces;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Rise.App.UserControls
+{
+    /// <summary>
+    /// A flyout that shows queued media. Supports displaying queues with
+    /// <see cref="IMediaItem"/>.
+    /// </summary>
+    public sealed partial class DefaultQueueFlyout : UserControl
+    {
+        public static readonly DependencyProperty QueuedItemsSourceProperty =
+            DependencyProperty.Register(nameof(QueuedItemsSource), typeof(object),
+                typeof(DefaultQueueFlyout), new PropertyMetadata(null));
+
+        public object QueuedItemsSource
+        {
+            get => GetValue(QueuedItemsSourceProperty);
+            set => SetValue(QueuedItemsSourceProperty, value);
+        }
+
+        public DefaultQueueFlyout()
+        {
+            InitializeComponent();
+        }
+    }
+}
