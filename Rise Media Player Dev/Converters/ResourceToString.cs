2a06b2f4020ec354a71ccc35677029bb84f39b63
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-08-27 21:33:07 -0500

    Fixed bugs in resource to string converter

--- Rise Media Player Dev/Converters/ResourceToString.cs
+++ Rise Media Player Dev/Converters/ResourceToString.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Rise.Common.Extensions.Markup;
 using Windows.ApplicationModel.Resources;
 using Windows.UI.Xaml.Data;
 
@@ -7,28 +8,23 @@ namespace Rise.App.Converters
     public class ResourceToString : IValueConverter
     {
         private static ResourceLoader _currLoader;
-        private static string _loader = "";
+        private static string _loader = string.Empty;
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             string resource = value.ToString();
-            if (parameter != null)
+            if (parameter is string param)
             {
-                string loader = parameter.ToString();
-                if (loader != _loader)
+                if (param != _loader)
                 {
-                    _loader = loader;
-                    _currLoader = ResourceLoader.GetForViewIndependentUse(loader);
+                    _loader = param;
+                    _currLoader = ResourceLoader.GetForViewIndependentUse(param);
                 }
 
-            }
-            else if (_loader != "")
-            {
-                _loader = "";
-                _currLoader = ResourceLoader.GetForViewIndependentUse();
+                return _currLoader.GetString(resource);
             }
 
-            return _currLoader.GetString(resource);
+            return ResourceHelper.GetString(resource);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
