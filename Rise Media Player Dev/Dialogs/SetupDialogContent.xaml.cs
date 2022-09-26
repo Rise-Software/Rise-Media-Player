a32c6df599b0ebeef0e0a546c67f86fd198b2a8d
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-09-25 20:49:10 -0500

    Reworked setup dialog into a page, started navigation rework

--- Rise Media Player Dev/Dialogs/SetupDialogContent.xaml.cs
+++ Rise Media Player Dev/Dialogs/SetupDialogContent.xaml.cs
@@ -7,39 +7,17 @@ using System.Threading.Tasks;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Media;
 
 namespace Rise.App.Dialogs
 {
-    public sealed partial class SetupDialog : ContentDialog
+    public sealed partial class SetupDialogContent : Page
     {
         private SettingsViewModel ViewModel => App.SViewModel;
 
-        #region Setup Icons
-        private readonly BitmapSource TermsImage =
-            new BitmapImage(new Uri("ms-appx:///Assets/Setup/Terms.png"));
-
-        private readonly BitmapSource ConnectImage =
-            new BitmapImage(new Uri("ms-appx:///Assets/Setup/Connect.png"));
-
-        private readonly BitmapSource LocalImage =
-            new BitmapImage(new Uri("ms-appx:///Assets/Setup/Local.png"));
-
-        private readonly BitmapSource PrivacyImage =
-            new BitmapImage(new Uri("ms-appx:///Assets/Setup/Privacy.png"));
-
-        private readonly BitmapSource AppearanceImage =
-            new BitmapImage(new Uri("ms-appx:///Assets/Setup/Appearance.png"));
-
-        private readonly BitmapSource DoneImage =
-            new BitmapImage(new Uri("ms-appx:///Assets/Setup/Done.png"));
-        #endregion
-
-        public SetupDialog()
+        public SetupDialogContent()
         {
             InitializeComponent();
-            ContentDialog_SizeChanged(null, null);
-
             Navigate();
         }
 
@@ -75,11 +53,10 @@ namespace Rise.App.Dialogs
             {
                 case 0:
                     ViewModel.SetupProgress--;
-                    Hide();
                     break;
 
                 case 1:
-                    ViewModel.SetupProgress++;
+                    ViewModel.FetchOnlineData = false;
                     break;
 
                 case 5:
@@ -87,9 +64,6 @@ namespace Rise.App.Dialogs
                     ViewModel.SetupProgress = 0;
 
                     _ = await ApplicationView.GetForCurrentView().TryConsolidateAsync();
-                    break;
-
-                default:
                     break;
             }
         }
@@ -102,137 +76,74 @@ namespace Rise.App.Dialogs
             BackButton.Visibility = ViewModel.SetupProgress > 1 ?
                 Visibility.Visible : Visibility.Collapsed;
 
-            ContentDialog_SizeChanged(null, null);
-
             switch (ViewModel.SetupProgress)
             {
-                case 0:
-                    Header.Text = ResourceLoaders.SetupLoader.GetString("LicenseH");
-                    PrimaryButton.Content = ResourceLoaders.SetupLoader.GetString("Accept");
-                    SecondaryButton.Content = ResourceLoaders.SetupLoader.GetString("Decline");
-                    SetupInfo.Text = ResourceLoaders.SetupLoader.GetString("PreSetup");
-
-                    SetupProgress.Value = 0;
-                    SetupIcon.Source = TermsImage;
-                    _ = SetupFrame.Navigate(typeof(TermsPage));
-                    break;
-
                 case 1:
-                    Header.Text = ResourceLoaders.SetupLoader.GetString("ConnectH");
                     PrimaryButton.Content = ResourceLoaders.SetupLoader.GetString("Continue");
                     SecondaryButton.Content = ResourceLoaders.SetupLoader.GetString("OnlyLocal");
                     SetupInfo.Text = ResourceLoaders.SetupLoader.GetString("Step1");
 
-                    SetupProgress.Value = 20;
-                    SetupIcon.Source = ConnectImage;
                     _ = SetupFrame.Navigate(typeof(ConnectPage));
                     break;
 
                 case 2:
-                    Header.Text = ResourceLoaders.SetupLoader.GetString("LocalH");
                     PrimaryButton.Content = ResourceLoaders.SetupLoader.GetString("Continue");
                     SecondaryButton.Content = ResourceLoaders.SetupLoader.GetString("OnlyStreaming");
                     SetupInfo.Text = ResourceLoaders.SetupLoader.GetString("Step2");
 
-                    SetupProgress.Value = 40;
-                    SetupIcon.Source = LocalImage;
                     _ = SetupFrame.Navigate(typeof(LocalPage));
                     break;
 
                 case 3:
-                    Header.Text = ResourceLoaders.SetupLoader.GetString("PrivacyH");
                     PrimaryButton.Content = ResourceLoaders.SetupLoader.GetString("Continue");
                     SecondaryButton.Content = ResourceLoaders.SetupLoader.GetString("Decide");
                     SetupInfo.Text = ResourceLoaders.SetupLoader.GetString("Step3");
 
-                    SetupProgress.Value = 60;
-                    SetupIcon.Source = PrivacyImage;
                     _ = SetupFrame.Navigate(typeof(PrivacyPage));
                     break;
 
                 case 4:
-                    Header.Text = ResourceLoaders.SetupLoader.GetString("AppearanceH");
                     PrimaryButton.Content = ResourceLoaders.SetupLoader.GetString("Continue");
                     SecondaryButton.Content = ResourceLoaders.SetupLoader.GetString("Decide");
                     SetupInfo.Text = ResourceLoaders.SetupLoader.GetString("Step4");
 
-                    SetupProgress.Value = 80;
-                    SetupIcon.Source = AppearanceImage;
                     _ = SetupFrame.Navigate(typeof(AppearancePage));
                     break;
 
                 case 5:
-                    Header.Text = ResourceLoaders.SetupLoader.GetString("DoneH");
                     PrimaryButton.Content = ResourceLoaders.SetupLoader.GetString("Continue");
                     SecondaryButton.Content = ResourceLoaders.SetupLoader.GetString("NotNow");
                     SetupInfo.Text = ResourceLoaders.SetupLoader.GetString("Step5");
 
-                    SetupProgress.Value = 100;
-                    SetupIcon.Source = DoneImage;
                     _ = SetupFrame.Navigate(typeof(FinishPage));
                     break;
 
                 case 6:
                     ViewModel.SetupCompleted = true;
                     ViewModel.SetupProgress = 0;
-                    Hide();
+
+                    var popups = VisualTreeHelper.GetOpenPopups(Window.Current);
+                    foreach (var popup in popups)
+                    {
+                        if (popup.Child is ContentDialog dialog)
+                        {
+                            dialog.Hide();
+                            break;
+                        }
+                    }
 
                     Frame rootFrame = Window.Current.Content as Frame;
                     _ = rootFrame.Navigate(typeof(MainPage));
                     break;
 
                 default:
-                    Header.Text = ResourceLoaders.SetupLoader.GetString("LicenseH");
                     PrimaryButton.Content = ResourceLoaders.SetupLoader.GetString("Accept");
                     SecondaryButton.Content = ResourceLoaders.SetupLoader.GetString("Decline");
                     SetupInfo.Text = ResourceLoaders.SetupLoader.GetString("PreSetup");
 
-                    SetupProgress.Value = 0;
-                    SetupIcon.Source = TermsImage;
                     _ = SetupFrame.Navigate(typeof(TermsPage));
                     break;
             }
-        }
-
-        private void ContentDialog_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            double windowWidth = Window.Current.Bounds.Width;
-            double windowHeight = Window.Current.Bounds.Height;
-
-            if (windowWidth < 770)
-            {
-                SetupFrame.Width = windowWidth - 68;
-                IconColumn.Width = new GridLength(0);
-                ProgressColumn.Width = new GridLength(0);
-                InfoGrid.ColumnSpacing = 0;
-                ControlGrid.Margin = new Thickness(-32, -24, -24, -24);
-
-                Header.Margin = BackButton.Visibility == Visibility.Visible ?
-                    new Thickness(42, -5, 0, 0) : new Thickness(0, -5, 0, 0);
-
-                if (LocalPage.Current != null)
-                {
-                    LocalPage.Current.SetupLocalFrame.Width = 450;
-                }
-            }
-            else
-            {
-                SetupFrame.Width = 770 - 284;
-                IconColumn.Width = new GridLength(188);
-                ProgressColumn.Width = new GridLength(210);
-                InfoGrid.ColumnSpacing = 28;
-                ControlGrid.Margin = new Thickness(-24);
-                Header.Margin = new Thickness(0, -4, 0, 0);
-
-                if (LocalPage.Current != null)
-                {
-                    LocalPage.Current.SetupLocalFrame.Width = 486;
-                }
-            }
-
-            // The 59 is because for some reason the dialog has a 1px transparent
-            // line at the bottom. Don't shoot me, I'm just the messenger.
-            RootGrid.Height = windowHeight < 498 ? windowHeight - 59 : 498 - 59;
         }
     }
 }
