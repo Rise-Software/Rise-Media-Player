c3ff76575795d4cf4132ec9cb3d5c8b715855fb0
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-09-26 23:24:58 -0500

    Reworked appearance page

--- Rise Media Player Dev/Setup/AppearancePage.xaml.cs
+++ Rise Media Player Dev/Setup/AppearancePage.xaml.cs
@@ -1,43 +1,12 @@
-﻿using Rise.App.ViewModels;
-using Rise.Common;
-using System;
-using System.Collections.Generic;
-using Windows.ApplicationModel.Core;
-using Windows.UI.Xaml.Controls;
-
+﻿using Windows.UI.Xaml.Controls;
 
 namespace Rise.App.Setup
 {
     public sealed partial class AppearancePage : Page
     {
-        private SettingsViewModel ViewModel => App.SViewModel;
-
-        private readonly List<string> Themes = new()
-        {
-            ResourceLoaders.AppearanceLoader.GetString("Light"),
-            ResourceLoaders.AppearanceLoader.GetString("Dark"),
-            ResourceLoaders.AppearanceLoader.GetString("System")
-        };
-
         public AppearancePage()
         {
             InitializeComponent();
-            ChangeThemeTip.IsOpen = false;
-
-
-            DataContext = ViewModel;
         }
-
-        private async void ChangeThemeTip_ActionButtonClick(Microsoft.UI.Xaml.Controls.TeachingTip sender, object args)
-        {
-            await CoreApplication.RequestRestartAsync("Theme changed");
-        }
-
-        private void ThemeChange_DropDownClosed(object sender, object e)
-        {
-            ChangeThemeTip.IsOpen = true;
-        }
-
-
     }
 }
