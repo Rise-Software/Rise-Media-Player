c3edf3733627042722730b9a1970b66184af3e5f
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-09-26 23:26:17 -0500

    Reworked privacy page

--- Rise Media Player Dev/Setup/PrivacyPage.xaml.cs
+++ Rise Media Player Dev/Setup/PrivacyPage.xaml.cs
@@ -1,16 +1,12 @@
-﻿using Rise.App.ViewModels;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace Rise.App.Setup
 {
     public sealed partial class PrivacyPage : Page
     {
-        private SettingsViewModel ViewModel => App.SViewModel;
-
         public PrivacyPage()
         {
             InitializeComponent();
-            DataContext = ViewModel;
         }
     }
 }
