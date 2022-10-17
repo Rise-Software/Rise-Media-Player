928ca58e2d11c481c17abdd3dacfb38de5c04316
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-10-16 22:33:50 -0500

    Added methods to get media from the web

--- Rise.Common/Helpers/WebHelpers.cs
+++ Rise.Common/Helpers/WebHelpers.cs
@@ -1,4 +1,10 @@
-﻿using Windows.Networking.Connectivity;
+﻿using Rise.Common.Constants;
+using System;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Networking.Connectivity;
+using Windows.Storage.Streams;
 
 namespace Rise.Common.Helpers
 {
@@ -14,6 +20,53 @@ namespace Rise.Common.Helpers
             ConnectionProfile connectionProfile = NetworkInformation.GetInternetConnectionProfile();
             return connectionProfile != null && connectionProfile.
                 GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="MediaPlaybackItem"/> from the provided song Uri.
+        /// </summary>
+        public static MediaPlaybackItem GetSongFromUri(Uri uri)
+        {
+            var media = GetMediaFromUri(uri, "Online song");
+            var props = media.GetDisplayProperties();
+
+            props.Type = MediaPlaybackType.Music;
+            props.MusicProperties.Title = "Online song";
+            props.MusicProperties.Artist = "UnknownArtistResource";
+            props.Thumbnail = RandomAccessStreamReference.CreateFromUri(new(URIs.MusicThumb));
+
+            media.ApplyDisplayProperties(props);
+            return media;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="MediaPlaybackItem"/> from the provided video Uri.
+        /// </summary>
+        public static MediaPlaybackItem GetVideoFromUri(Uri uri)
+        {
+            var media = GetMediaFromUri(uri, "Online video");
+            var props = media.GetDisplayProperties();
+
+            props.Type = MediaPlaybackType.Video;
+            props.VideoProperties.Title = "Online video";
+            props.VideoProperties.Subtitle = "UnknownArtistResource";
+            props.Thumbnail = RandomAccessStreamReference.CreateFromUri(new(URIs.MusicThumb));
+
+            media.ApplyDisplayProperties(props);
+            return media;
+        }
+
+        private static MediaPlaybackItem GetMediaFromUri(Uri uri, string title)
+        {
+            var source = MediaSource.CreateFromUri(uri);
+
+            source.CustomProperties["Title"] = title;
+            source.CustomProperties["Artists"] = "UnknownArtistResource";
+            source.CustomProperties["Length"] = source.Duration;
+            source.CustomProperties["Year"] = 0u;
+
+            var media = new MediaPlaybackItem(source);
+            return media;
         }
     }
 }
