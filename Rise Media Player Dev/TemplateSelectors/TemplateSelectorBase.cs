f8e6ac3d15ec496887d137b34aac639c0ca69b5e
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-10-18 16:32:00 -0500

    Added base class for template selectors

---
+++ Rise Media Player Dev/TemplateSelectors/TemplateSelectorBase.cs
@@ -0,0 +1,43 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Rise.App.TemplateSelectors
+{
+    /// <summary>
+    /// A type safe base class for template selectors. Allows
+    /// specifying a type for the item parameter in the base
+    /// SelectTemplateCore overrides.
+    /// </summary>
+    internal abstract class TemplateSelectorBase<TItem> : DataTemplateSelector
+    {
+        protected sealed override DataTemplate SelectTemplateCore(object item)
+            => SelectTemplateCore((TItem)item);
+
+        protected sealed override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+            => SelectTemplateCore((TItem)item, container);
+
+        protected virtual DataTemplate SelectTemplateCore(TItem item)
+            => base.SelectTemplateCore(item);
+    }
+
+    /// <summary>
+    /// A type safe base class for template selectors. Allows
+    /// specifying a type for the item and container parameters
+    /// in the base SelectTemplateCore overrides.
+    /// </summary>
+    internal abstract class TemplateSelectorBase<TItem, TContainer> : DataTemplateSelector
+        where TContainer : DependencyObject
+    {
+        protected sealed override DataTemplate SelectTemplateCore(object item)
+            => SelectTemplateCore((TItem)item);
+
+        protected sealed override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+            => SelectTemplateCore((TItem)item, (TContainer)container);
+
+        protected virtual DataTemplate SelectTemplateCore(TItem item)
+            => base.SelectTemplateCore(item);
+
+        protected virtual DataTemplate SelectTemplateCore(TItem item, TContainer container)
+            => base.SelectTemplateCore(item, container);
+    }
+}
