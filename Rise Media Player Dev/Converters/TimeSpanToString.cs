076bec2c7a968659479d2942c02e0432090c2f2e
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-11-02 09:11:53 -0500

    Improved TimeSpanToString converter

--- Rise Media Player Dev/Converters/TimeSpanToString.cs
+++ Rise Media Player Dev/Converters/TimeSpanToString.cs
@@ -1,122 +1,119 @@
-﻿using System;
+﻿using Rise.Common.Extensions.Markup;
+using System;
+using System.Text;
 using Windows.UI.Xaml.Data;
 
 namespace Rise.App.Converters
 {
-    public class TimeSpanToString : IValueConverter
+    public sealed partial class TimeSpanToString : IValueConverter
     {
-        public static string Convert(TimeSpan value, string param = null)
-        {
-            if (param == null)
-            {
-                if (value.TotalHours >= 1 && value.TotalHours <= 9)
-                {
-                    return value.ToString("h\\:mm\\:ss");
-                }
-                else if (value.TotalHours >= 10)
-                {
-                    return value.ToString("hh\\:mm\\:ss");
-                }
-                else if (value.TotalMinutes >= 10)
-                {
-                    return value.ToString("mm\\:ss");
-                }
-                else
-                {
-                    return value.ToString("m\\:ss");
-                }
-            }
-
-            string time = string.Empty;
-            switch (param)
-            {
-                case "D-S":
-                    if (value.Days > 0)
-                    {
-                        if (value.Days == 1)
-                        {
-                            time += value.Days + " day, ";
-                        }
-                        else
-                        {
-                            time += value.Days + " days, ";
-                        }
-                    }
-
-                    if (value.Hours > 0)
-                    {
-                        if (value.Hours == 1)
-                        {
-                            time += value.Hours + " hour, ";
-                        }
-                        else
-                        {
-                            time += value.Hours + " hours, ";
-                        }
-                    }
-
-                    if (value.Minutes > 0)
-                    {
-                        if (value.Minutes == 1)
-                        {
-                            time += value.Minutes + " minute, ";
-                        }
-                        else
-                        {
-                            time += value.Minutes + " minutes, ";
-                        }
-                    }
-
-                    if (value.Seconds == 1)
-                    {
-                        time += value.Seconds + " second";
-                    }
-                    else
-                    {
-                        time += value.Seconds + " seconds";
-                    }
-                    break;
-
-                default:
-                    time = value.ToString("mm\\:ss");
-                    break;
-            }
-
-            return time;
-        }
-
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var span = value is TimeSpan time ?
                 time : TimeSpan.Zero;
 
-            if (parameter == null)
-            {
-                if (span.TotalHours >= 1 && span.TotalHours <= 9)
-                {
-                    return span.ToString("h\\:mm\\:ss");
-                }
-                else if (span.TotalHours >= 10)
-                {
-                    return span.ToString("hh\\:mm\\:ss");
-                }
-                else if (span.TotalMinutes >= 10)
-                {
-                    return span.ToString("mm\\:ss");
-                }
-                else
-                {
-                    return span.ToString("m\\:ss");
-                }
+            string param = parameter?.ToString();
+            if (string.IsNullOrEmpty(param))
+                return GetShortFormat(span);
 
-            }
-
-            return Convert(span, parameter.ToString());
+            return GetLongFormat(ref span, param);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    // Converter logic
+    public sealed partial class TimeSpanToString
+    {
+        public static string GetLongFormat(TimeSpan value, string format)
+            => GetLongFormat(ref value, format);
+
+        public static string GetShortFormat(TimeSpan span)
+            => GetShortFormat(ref span);
+
+        private static string GetLongFormat(ref TimeSpan value, string format)
+        {
+            var timeBuilder = new StringBuilder();
+            switch (format[0])
+            {
+                case 'D':
+                    if (value.Days == 1)
+                    {
+                        timeBuilder.Append(ResourceHelper.GetString("OneDay"));
+                        timeBuilder.Append(", ");
+                    }
+                    else if (value.Days > 0)
+                    {
+                        string formatStr = ResourceHelper.GetString("NDays");
+                        timeBuilder.Append(string.Format(formatStr, value.Days));
+                        timeBuilder.Append(", ");
+                    }
+
+                    if (format[2] != 'D') goto case 'H';
+                    break;
+
+                case 'H':
+                    if (value.Hours == 1)
+                    {
+                        timeBuilder.Append(ResourceHelper.GetString("OneHour"));
+                        timeBuilder.Append(", ");
+                    }
+                    else if (value.Hours > 0)
+                    {
+                        string formatStr = ResourceHelper.GetString("NHours");
+                        timeBuilder.Append(string.Format(formatStr, value.Hours));
+                        timeBuilder.Append(", ");
+                    }
+
+                    if (format[2] != 'H') goto case 'M';
+                    break;
+
+                case 'M':
+                    if (value.Minutes == 1)
+                    {
+                        timeBuilder.Append(ResourceHelper.GetString("OneMinute"));
+                        timeBuilder.Append(", ");
+                    }
+                    else if (value.Minutes > 0)
+                    {
+                        string formatStr = ResourceHelper.GetString("NMinutes");
+                        timeBuilder.Append(string.Format(formatStr, value.Minutes));
+                        timeBuilder.Append(", ");
+                    }
+
+                    if (format[2] != 'M') goto case 'S';
+                    break;
+
+                case 'S':
+                    if (value.Seconds == 1)
+                    {
+                        timeBuilder.Append(ResourceHelper.GetString("OneSecond"));
+                    }
+                    else if (value.Seconds > 0)
+                    {
+                        string formatStr = ResourceHelper.GetString("NSeconds");
+                        timeBuilder.Append(string.Format(formatStr, value.Seconds));
+                    }
+                    break;
+            }
+            return timeBuilder.ToString();
+        }
+
+        private static string GetShortFormat(ref TimeSpan span)
+        {
+            if (span.Days >= 1)
+                return span.ToString("d.\\hh\\:mm\\:ss");
+            else if (span.Hours >= 1 && span.Hours <= 9)
+                return span.ToString("h\\:mm\\:ss");
+            else if (span.Hours >= 10)
+                return span.ToString("hh\\:mm\\:ss");
+            else if (span.Minutes >= 10)
+                return span.ToString("mm\\:ss");
+            else
+                return span.ToString("m\\:ss");
         }
     }
 }
