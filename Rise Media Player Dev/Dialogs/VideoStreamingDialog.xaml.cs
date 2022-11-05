18bcced56083d68d593b4379e93b8a7b93767f82
SimpleBear <itswin11@outlook.com>
2022-11-05 18:35:13 +0200

    Add YouTube support for URL playback, fix problems with M3U/DASH streaming

--- Rise Media Player Dev/Dialogs/VideoStreamingDialog.xaml.cs
+++ Rise Media Player Dev/Dialogs/VideoStreamingDialog.xaml.cs
@@ -2,9 +2,13 @@
 using Rise.Data.ViewModels;
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Net;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Videos.Streams;
 
 namespace Rise.App.Dialogs
 {
@@ -19,33 +23,36 @@ namespace Rise.App.Dialogs
 
         private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            string url = StreamingTextBox.Text;
+            var url = StreamingTextBox.Text;
 
-            bool isValidVideo = false;
-            try
-            {
-                var req = WebRequest.Create(url);
-                req.Method = "HEAD";
+            string title = null, subtitle = null, thumbnailUrl = null;
 
-                using var resp = req.GetResponse();
-                isValidVideo = resp.ContentType.ToLower(CultureInfo.InvariantCulture)
-                    .StartsWith("video/", StringComparison.OrdinalIgnoreCase);
-            }
-            catch { }
-
-            if (!isValidVideo || !Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            if (!Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out Uri uri))
             {
                 args.Cancel = true;
                 InvalidUrlText.Visibility = Visibility.Visible;
                 return;
             }
 
-            // Well formed URL (if it isn't we've already returned anyways)
+            if (url.Contains("youtube.com/watch"))
+            {
+                var youtubeClient = new YoutubeClient();
+                var youtubeVideo = await youtubeClient.Videos.GetAsync(url.Replace("music.youtube.com", "www.youtube.com"));
+
+                title = youtubeVideo.Title;
+                subtitle = youtubeVideo.Author.ChannelTitle;
+                thumbnailUrl = youtubeVideo.Thumbnails.GetWithHighestResolution().Url;
+
+                var streams = await youtubeClient.Videos.Streams.GetManifestAsync(url);
+
+                uri = new(streams.GetMuxedStreams().GetWithHighestVideoQuality().Url);
+            }
+
             Hide();
 
             await ViewModel.ResetPlaybackAsync();
 
-            var video = WebHelpers.GetVideoFromUri(new(url));
+            var video = WebHelpers.GetVideoFromUri(uri, title, subtitle, thumbnailUrl);
             ViewModel.AddSingleItemToQueue(video);
             ViewModel.Player.Play();
         }
