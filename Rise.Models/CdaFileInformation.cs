4abea546ae38ab893842e5487cea100ae9b02116
SimpleBear <itswin11@outlook.com>
2022-12-27 09:44:09 +0200

    Added model to read CDA files

---
+++ Rise.Models/CdaFileInformation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace Rise.Models
+{
+    public class CdaFileInformation
+    {
+        public string Identifier { get; } = "CDDA";
+
+        public TimeSpan RangePosition { get; private set; }
+
+        public TimeSpan Duration { get; private set; }
+
+        public static async Task<CdaFileInformation> GetFromFileAsync(IStorageFile file)
+        {
+            using var stream = await file.OpenReadAsync();
+            var basicProperties = await file.GetBasicPropertiesAsync();
+
+            byte[] buffer = new byte[basicProperties.Size];
+
+            _ = await stream.ReadAsync(buffer.AsBuffer(), (uint)buffer.Length, InputStreamOptions.None);
+
+            return new CdaFileInformation()
+            {
+                RangePosition = new TimeSpan(0, buffer[0x26], buffer[0x25]),
+                Duration = new TimeSpan(0, buffer[0x2a], buffer[0x29])
+            };
+        }
+    }
+}
