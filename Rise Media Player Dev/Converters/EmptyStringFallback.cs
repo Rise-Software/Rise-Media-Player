07d086bb4dfef06b55907ac85e939e476b089138
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2023-02-26 20:13:36 -0500

    Added converter to use a fallback with empty strings

---
+++ Rise Media Player Dev/Converters/EmptyStringFallback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Rise.App.Converters;
+
+/// <summary>
+/// Returns the provided value if it's not an empty string,
+/// returns the converter parameter otherwise.
+/// </summary>
+public sealed class EmptyStringFallback : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (string.IsNullOrEmpty((string)value))
+            return parameter;
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
