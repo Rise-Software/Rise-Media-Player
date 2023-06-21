2e9c4508130ab459cf3e3b6ca877de6254c84848
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2023-06-21 15:29:27 -0500

    Fixed localization of glaze color names

--- Rise Media Player Dev/Settings/AppearancePage.xaml.cs
+++ Rise Media Player Dev/Settings/AppearancePage.xaml.cs
@@ -55,57 +55,62 @@ namespace Rise.App.Settings
 
         private void PopulateColors()
         {
-            AddColor("Yellow gold", 255, 185, 0);
-            AddColor("Gold", 255, 140, 0);
-            AddColor("Bright orange", 247, 99, 12);
-            AddColor("Dark orange", 202, 80, 16);
-            AddColor("Rust", 218, 59, 1);
-            AddColor("Pale rust", 239, 105, 80);
-            AddColor("Brick red", 209, 52, 56);
-            AddColor("Mod red", 255, 67, 67);
-            AddColor("Pale red", 231, 72, 86);
-            AddColor("Red", 232, 17, 35);
-            AddColor("Rose bright", 234, 0, 94);
-            AddColor("Rose", 195, 0, 82);
-            AddColor("Plum light", 227, 0, 140);
-            AddColor("Plum", 191, 0, 119);
-            AddColor("Orchid light", 194, 57, 179);
-            AddColor("Orchid", 154, 0, 137);
-            AddColor("Blue", 0, 120, 212);
-            AddColor("Navy blue", 0, 99, 177);
-            AddColor("Purple shadow", 142, 140, 216);
-            AddColor("Purple shadow dark", 107, 105, 214);
-            AddColor("Iris pastel", 135, 100, 184);
-            AddColor("Iris spring", 116, 77, 169);
-            AddColor("Violet red light", 177, 70, 194);
-            AddColor("Violet red", 136, 23, 152);
-            AddColor("Cool blue bright", 0, 153, 188);
-            AddColor("Cool blue", 45, 125, 154);
-            AddColor("Seafoam", 0, 183, 195);
-            AddColor("Seafoam teal", 3, 131, 135);
-            AddColor("Mint light", 0, 178, 148);
-            AddColor("Mint dark", 1, 133, 116);
-            AddColor("Turf green", 0, 204, 106);
-            AddColor("Sport green", 16, 137, 62);
-            AddColor("Grey", 122, 117, 116);
-            AddColor("Grey brown", 93, 90, 88);
-            AddColor("Steel blue", 104, 118, 138);
-            AddColor("Metal blue", 81, 92, 107);
-            AddColor("Pale moss", 86, 124, 116);
-            AddColor("Moss", 72, 104, 96);
-            AddColor("Meadow green", 73, 130, 5);
-            AddColor("Green", 16, 124, 16);
-            AddColor("Overcast", 118, 118, 118);
-            AddColor("Storm", 76, 74, 72);
-            AddColor("Blue grey", 105, 121, 126);
-            AddColor("Grey dark", 74, 84, 89);
-            AddColor("Liddy green", 100, 124, 100);
-            AddColor("Sage", 82, 94, 84);
-            AddColor("Camouflage desert", 132, 117, 69);
-            AddColor("Camouflage", 126, 115, 95);
+            AddColor(255, 185, 0);
+            AddColor(255, 140, 0);
+            AddColor(247, 99, 12);
+            AddColor(202, 80, 16);
+            AddColor(218, 59, 1);
+            AddColor(239, 105, 80);
+            AddColor(209, 52, 56);
+            AddColor(255, 67, 67);
+            AddColor(231, 72, 86);
+            AddColor(232, 17, 35);
+            AddColor(234, 0, 94);
+            AddColor(195, 0, 82);
+            AddColor(227, 0, 140);
+            AddColor(191, 0, 119);
+            AddColor(194, 57, 179);
+            AddColor(154, 0, 137);
+            AddColor(0, 120, 212);
+            AddColor(0, 99, 177);
+            AddColor(142, 140, 216);
+            AddColor(107, 105, 214);
+            AddColor(135, 100, 184);
+            AddColor(116, 77, 169);
+            AddColor(177, 70, 194);
+            AddColor(136, 23, 152);
+            AddColor(0, 153, 188);
+            AddColor(45, 125, 154);
+            AddColor(0, 183, 195);
+            AddColor(3, 131, 135);
+            AddColor(0, 178, 148);
+            AddColor(1, 133, 116);
+            AddColor(0, 204, 106);
+            AddColor(16, 137, 62);
+            AddColor(122, 117, 116);
+            AddColor(93, 90, 88);
+            AddColor(104, 118, 138);
+            AddColor(81, 92, 107);
+            AddColor(86, 124, 116);
+            AddColor(72, 104, 96);
+            AddColor(73, 130, 5);
+            AddColor(16, 124, 16);
+            AddColor(118, 118, 118);
+            AddColor(76, 74, 72);
+            AddColor(105, 121, 126);
+            AddColor(74, 84, 89);
+            AddColor(100, 124, 100);
+            AddColor(82, 94, 84);
+            AddColor(132, 117, 69);
+            AddColor(126, 115, 95);
 
-            static void AddColor(string name, byte r, byte g, byte b)
-                => _glazeColors.Add(new(name, r, g, b));
+            static void AddColor(byte r, byte g, byte b)
+            {
+                var color = Color.FromArgb(255, r, g, b);
+                string name = ColorHelper.ToDisplayName(color);
+
+                _glazeColors.Add(new(name, color));
+            }
         }
 
         private void ExpanderControl_Loaded(object sender, RoutedEventArgs e)
